23f7603541c349cb0b581d4d64e482dd81a249d2
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-10-20 07:47:05 -0500

    feat: :seedling: Create Files
    Create files for entities, interfaces, configuration, repositories, controllers and Dtos
    
    @duvanrg

---
+++ Core/Entities/Departamento.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Departamento : BaseEntity
+    {
+        public string Nombre { get; set; }
+        
+        
+    }
+}
